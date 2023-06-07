8fb06bc1492642fef7346d0126c6ce1ad2130559
souvanny <ssisengrath@hotmail.com>
2023-06-07 13:38:45 +0200

    step 5

--- front/src/app/screens/contribute-details/contribute-details.component.ts
+++ front/src/app/screens/contribute-details/contribute-details.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router'
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-contribute-details',
@@ -7,4 +10,28 @@ import { Component } from '@angular/core';
 })
 export class ContributeDetailsComponent {
 
+  answers:  { title: String }[] = [];
+
+  title: String = "";
+  hashtags: String = "";
+
+  constructor(private route: ActivatedRoute, private http: HttpClient, private router: Router) { }
+
+  ngOnInit() {
+    console.log("ContributeDetailsComponent init ============");
+    this.loadData();
+  }
+
+  loadData() {
+    this.http.get<any>('http://localhost:8080/api/questions/26').subscribe(data => {
+      console.log("Get done");
+      console.log(data);
+
+      this.title = data.title;
+      this.hashtags = data.hashtags;
+      this.answers = data.answers;
+      
+
+     })
+  }
 }
